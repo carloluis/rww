a2d392c333db5adbf95d0e6e181c636319bdd695
carloluis <carloluisr@gmail.com>
2016-09-14 00:24:48 -0300

    update webpack.config -> reduce bundle size to 146kb

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,19 @@
 let production = process.argv.indexOf('-p') > -1? true: false;
 const NODE_ENV = production? 'production': 'development';
 
+const webpack = require('webpack');
+
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
     template: __dirname + '/app/index.html',
     filename: 'index.html',
     inject: 'body'
+});
+
+var EnvPluginConfig = new webpack.DefinePlugin({
+    'process.env':{
+        'NODE_ENV': JSON.stringify(NODE_ENV)
+    }
 });
 
 module.exports = {
@@ -23,8 +31,9 @@ module.exports = {
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['react', 'es2015'] } }
         ]
     },
-    plugins: [HTMLWebpackPluginConfig],
+    plugins: [HTMLWebpackPluginConfig, EnvPluginConfig],
     devtool: '#source-map',
+    //devtool: 'cheap-module-source-map',
     devServer: { inline: true },
     watch: true
 }

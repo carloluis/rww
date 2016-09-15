6c971446cf7d7656242054654b085c4a5a598a61
carloluis <carloluisr@gmail.com>
2016-09-14 23:49:20 -0300

    update hello.js import in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import Hello from 'Hello'
+import Hello from './hello'
 
 ReactDOM.render(
 	<Hello message="Hello RWW" />, 

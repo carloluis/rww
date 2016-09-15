449652fd6b97cfb5420710d6035b83811c8e0715
carloluis <carloluisr@gmail.com>
2016-09-15 18:02:58 -0300

    add style script

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import ReactRouter, {Link} from 'react-router';
+import ReactRouter, {Link} from 'react-router'
+import styles from '../styles/index'
 
 class Home extends Component{
 	render(){
         return (
-            <div className='jumbotron col-sm-12 text-center'>
+            <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
                 <h1>Battle</h1>
                 <p className='lead'>Some fancy motto</p>
                 <Link to='/playerOne'>

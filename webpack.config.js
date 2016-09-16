4a67e0914c525c468f628e4909228634c2fabbed
carloluis <carloluisr@gmail.com>
2016-09-15 23:31:05 -0300

    add jshint pre-loader (dev build config)

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,13 @@ module.exports = {
         sourceMapFilename: '[file].map'
     },
     module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'jshint-loader'
+            }
+        ],
         loaders: [
             { 
             	test: /\.js$/, 
